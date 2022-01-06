b9ef02709052effa3ca4c1bbba9944ed03ff6b92
laksh <lakshyam38@gmail.com>
2022-01-06 16:46:16 +0530

    header completed

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>TENFLIX</h1>
+            <h3 className={styles.subheading}>
+                Get top 10 similar sugesstions based on the plot, genre, cast, and directors of your favorite Netflix title.
+            </h3>
+        </header>
+    )
+}
+
+export default Header
--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,24 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import Header from './Header'
 
 const Layout = ({ children }) => {
     return (
         <>
             <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-            name="description"
-            content="Get top 10 Netflix recommendations based on your favorite Netflix titles!"
-            />
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                name="description"
+                content="Get top 10 Netflix recommendations based on your favorite Netflix titles!"
+                />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
             </Head>
+            <Header />
             <div className={styles.container}>
                 <main className={styles.container}>
                     {children}
